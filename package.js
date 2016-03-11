fca145c54c8817d651d7ae31c7d0aec137206971
Daniel Dornhardt <daniel@dornhardt.com>
2016-03-11 18:50:39 +0100

    Bumping Package Version

--- package.js
+++ package.js
@@ -1,7 +1,7 @@
 Package.describe({
   name: 'nicolaslopezj:router-layer',
   summary: 'A layer for Meteor Routers',
-  version: '0.0.11',
+  version: '0.0.12',
   git: 'https://github.com/nicolaslopezj/meteor-router-layer'
 });
 
